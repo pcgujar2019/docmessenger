5befcd1930c3e6a4f8b567eca03cc495435601df
pcgujar2019 <gujarprashant89@gmail.com>
2021-08-16 15:24:10 +0530

    age--

--- appointments/forms.py
+++ appointments/forms.py
@@ -1,18 +1,34 @@
 from django.forms import ModelForm, widgets
 from .models import Appointment
+from django import forms
 
 class AppointmentForm(ModelForm):
+    # on_date = forms.DateField(widget=DatePicker())
+    # at_time = forms.TimeField(
+    #     widget=TimePicker(
+    #         options={
+    #             'enabledHours': [9, 10, 11, 12, 13, 14, 15, 16],
+    #             'defaultDate': '1970-01-01T14:56:00'
+    #         },
+    #         attrs={
+    #             'input_toggle': True,
+    #             'input_group': False,
+    #         },
+    #     ),
+    # )
+    # note = forms.CharField(max_length=500)
+    
     class Meta:
         model = Appointment
         fields = '__all__'
-        exclude = ['date_added']
+        exclude = ['date_added', 'patient']
         labels = {
             'on_date': 'Date',
-            'at_time': 'Time',
-            'patient': 'Select Patient For Appointment',
-            'note': 'Note (optional)'
+            'note': 'Note (optional)',
+            'patient_name': 'Select Patient For Appointment',
         }
-        widgets = {
-            'on_date': widgets.DateInput(format= '%d-%m-%Y', attrs={'class':'form-control', 'autocomplete':'off', 'placeholder': 'dd-mm-yyyy'}),
-            'at_time': widgets.TimeInput(format='HH:MM', attrs={'class': 'form-control', 'autocomplete': 'off', 'placeholder': 'HH:MM'}),
+        widgets = { 
+            'on_date': widgets.DateInput(format='%d-%m-%Y', attrs={ 'type':'date', 'class':'form-control', 'autocomplete':'off', 'placeholder': 'dd-mm-yyyy'}),  
          }
+
+
--- appointments/views.py
+++ appointments/views.py
@@ -2,6 +2,9 @@ from appointments.models import Appointment
 from appointments.forms import AppointmentForm
 from django.shortcuts import render
 from django.contrib import messages
+from patients.models import Patient
+from django.http import JsonResponse
+
 
 # Create your views here.
 def addAppointment(request):
@@ -13,14 +16,27 @@ def addAppointment(request):
     elif request.method=='POST':
         form = AppointmentForm(request.POST) 
         try:
-            if form.is_valid():
-                form.save()
-                messages.add_message(request, messages.SUCCESS, 'Appointment Booked Successfully!')
-                return render(request, 'appointments/add-appointment.html', {'form': AppointmentForm()})
-            else:
-                messages.add_message(request, messages.ERROR, 'Please enter valid data!')
-                return render (request, 'patients/add-appointment.html', {'form':AppointmentForm()})
-        except ValueError:
-            messages.add_message(request, messages.ERROR, 'Please submit valid data!')
-            return render(request, 'main/add-appointment.html', {'form':form})
+            patientName = request.POST['patient_name']
+            date = request.POST['on_date']
+            print(date)
+            newApt = form.save(commit=False)
+            patientName = request.POST['patient_name']
+            patient = Patient.objects.get(patient_name=patientName)
+            newApt.patient = patient  
+            messages.add_message(request, messages.SUCCESS, 'Appointment Booked Successfully!')
+            return render(request, 'appointments/add-appointment.html', {'form': AppointmentForm()})
+        except Exception as e:
+            messages.add_message(request, messages.ERROR, e)
+            return render(request, 'appointments/add-appointment.html', {'form':form})
     return render(request, 'appointments/add-appointment.html', {})
+
+def allAppointments(request):
+    return render(request, 'appointments/all-appointments.html', {})
+
+def fetchPatients(request):
+    if 'term' in request.GET:
+        patients = Patient.objects.filter(full_name__icontains=request.GET.get('term'))
+        names = list()
+        for patient in patients:
+            names.append(patient.full_name)
+        return JsonResponse(names, safe=False)
