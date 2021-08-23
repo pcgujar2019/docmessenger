4596f8e88ce3d452165ed9f731608d4d74a898c5
pcgujar2019 <gujarprashant89@gmail.com>
2021-08-23 19:32:18 +0530

    23rd Aug

--- appointments/views.py
+++ appointments/views.py
@@ -1,9 +1,11 @@
+from time import time
 from appointments.models import Appointment
 from appointments.forms import AppointmentForm
 from django.shortcuts import render
 from django.contrib import messages
 from patients.models import Patient
 from django.http import JsonResponse
+from datetime import date, datetime, timedelta
 
 
 # Create your views here.
@@ -30,9 +32,28 @@ def addAppointment(request):
             messages.add_message(request, messages.ERROR, e)
             return render(request, 'appointments/add-appointment.html', {'form':form})
 
+# This shows the today's, tomorrow's apoointments. Also datepicker to search appointment on a particular day
 def allAppointments(request):
-    return render(request, 'appointments/all-appointments.html', {})
+    todayDate = datetime.now()
+    # Get today's appointments
+    todayApts = Appointment.objects.filter(on_date=todayDate)
+    #  Get tomorrow's appointments
+    tomorrowApts = Appointment.objects.filter(on_date=(todayDate + timedelta(1)))
+    if request.method == 'GET':
+        return render(request, 'appointments/all-appointments.html', 
+                {'todayApts': todayApts, 'tomorrowApts': tomorrowApts, 'today': todayDate, 'tomorrow': todayDate + timedelta(1)})
 
+    elif 'searchByDate' in request.POST:
+        date = request.POST['dateInput']
+        aptsOnDate = Appointment.objects.filter(on_date=date)
+        return render(request, 'appointments/all-appointments.html', 
+                {'todayApts': todayApts, 
+                'tomorrowApts': tomorrowApts, 
+                'today': todayDate, 
+                'tomorrow': todayDate + timedelta(1), 
+                'aptsOnDate': aptsOnDate,
+                'onDate': date})
+        
 def fetchPatients(request):
     if 'term' in request.GET:
         patients = Patient.objects.filter(full_name__icontains=request.GET.get('term'))
